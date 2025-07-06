06138615d099a9d180813e8f68211ce0f85842e5
john lloyd <johnlloyd@apollo.com.ph>
2025-07-06 19:42:36 +0800

    Initial commit

---
+++ src/components/Expertise.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import Chip from '@mui/material/Chip';
+import '../assets/styles/Expertise.scss';
+
+const labelsFirst = [
+    "TypeScript",
+    "JavaScript",
+    "HTML5",
+    "CSS3",
+    "SASS",
+    "Vue 3",
+    "Quasar Framework",
+    "SQL",
+    "Postman",
+    "Java",
+    "Spring Boot",
+    "C++",
+];
+
+const labelsSecond = [
+    "Git",
+    "GitHub Actions",
+    "Docker",
+    "Linux",
+    "Cypress",
+    "jest",
+];
+
+const labelsThird = [
+    "OpenAI",
+    "Groq",
+    "LangChain",
+    "Qdrant",
+    "Hugging Face",
+    "LlamaIndex",
+    "Streamlit",
+];
+
+function Expertise() {
+    return (
+    <div className="container" id="expertise">
+        <div className="skills-container">
+            <h1>Expertise</h1>
+            <div className="skills-grid">
+                <div className="skill">
+                    <FontAwesomeIcon icon={faReact} size="3x"/>
+                    <h3>Full Stack Web Development</h3>
+                    <p>Dynamic Full Stack Web Developer experienced in building responsive, end‑to‑end applications using modern stacks like Vue 3, Quasar, and Spring Boot. Skilled in both frontend and backend development, I thrive in the full Software Development Life Cycle—from initial planning and API design to deployment and maintenance. My versatile background ensures seamless UX design, robust backend logic, and efficient database management.</p>
+                    <div className="flex-chips">
+                        <span className="chip-title">Tech stack:</span>
+                        {labelsFirst.map((label, index) => (
+                            <Chip key={index} className='chip' label={label} />
+                        ))}
+                    </div>
+                </div>
+
+                <div className="skill">
+                    <FontAwesomeIcon icon={faDocker} size="3x"/>
+                    <h3>DevOps & Automation</h3>
+                    <p>After development, I partner with clients to architect and implement robust DevOps practices—setting up automated testing, continuous integration and delivery pipelines, and deployment automation. This ensures reliable, repeatable releases and smooth Go‑Live transitions.</p>
+                    <div className="flex-chips">
+                        <span className="chip-title">Tech stack:</span>
+                        {labelsSecond.map((label, index) => (
+                            <Chip key={index} className='chip' label={label} />
+                        ))}
+                    </div>
+                </div>
+
+                <div className="skill">
+                    <FontAwesomeIcon icon={faPython} size="3x"/>
+                    <h3>GenAI & LLM</h3>
+                    <p>Stay relevant in the market by leveraging the latest AI models in your projects. I have professional experience building enterprise grade GenAI-enabled solutions to empower intelligent decision making.</p>
+                    <div className="flex-chips">
+                        <span className="chip-title">Tech stack:</span>
+                        {labelsThird.map((label, index) => (
+                            <Chip key={index} className='chip' label={label} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Expertise;
